c871bcda28683029a355cfb55d72e023f56bf002
gzj-creator <1790188287@qq.com>
2026-03-01 02:16:16 +0800

    功能（HTTP2）：新增确定性入站帧分发器

--- galay-http/kernel/http2/Http2ConnectionCore.cc
+++ galay-http/kernel/http2/Http2ConnectionCore.cc
@@ -1,4 +1,5 @@
 #include "Http2ConnectionCore.h"
+#include "Http2FrameDispatcher.h"
 #include "galay-kernel/common/Sleep.hpp"
 
 namespace galay::http2
@@ -6,9 +7,12 @@ namespace galay::http2
 
 galay::kernel::Coroutine Http2ConnectionCore::run()
 {
+    H2DispatcherConnectionState dispatch_state;
     m_state.store(State::Running, std::memory_order_release);
     while (!m_stop_requested.load(std::memory_order_acquire)) {
         // Skeleton loop: concrete read/dispatch/schedule stages will be added in later tasks.
+        // Keep dispatcher state alive so later tasks can wire in real frames.
+        (void)dispatch_state;
         co_await galay::kernel::sleep(std::chrono::milliseconds(1));
     }
     m_state.store(State::Stopped, std::memory_order_release);
